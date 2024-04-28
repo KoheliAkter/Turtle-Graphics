b419923393e85ac6c84cff485a8c86798f9338e0
Koheli akter <koheliakter344@gmail.com>
2024-04-28 22:47:29 +0600

    done project

---
+++ Spiral of squares.py
@@ -0,0 +1,15 @@
+from turtle import *
+
+pencolor("#FFBF00")
+bgcolor("black")
+speed(0)
+
+for i in range(10,240):
+    for j in range(4):
+        fd(i)
+        right(90)
+
+    right(10)    
+
+
+done()
